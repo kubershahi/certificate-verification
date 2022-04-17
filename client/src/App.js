b2eb9dea7398a95f21e79147800b631e310fbeb6
Kuber Shahi <shahikuber97@gmail.com>
2022-04-17 21:37:32 +0530

    reset

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,34 @@
+import './App.css';
+
 import React from "react";
 import { DrizzleContext } from "@drizzle/react-plugin";
 import { newContextComponents } from "@drizzle/react-components";
 const { AccountData, ContractData, ContractForm } = newContextComponents;
 
+// function App() {
+//   <DrizzleContext.Consumer>
+//     {drizzleContext => {
+//       const { drizzle, drizzleState, initialized } = drizzleContext;      
+//       if (!initialized) {
+//         return "Loading...";
+//       }
+//       return (
+//         <h1>Hello</h1>
+//         // console.log((Object.entries(drizzleState.accounts).length))
+//         //console.log(drizzleState)
+//         // <div>
+//         // <AccountData drizzle={drizzle} drizzleState={drizzleState} accountIndex={0} units="ether" precision={3}/>
+//         // {/* <ContractData drizzle={drizzle} drizzleState={drizzleState} contract="Hello" method="get"/> */}
+//         // <ContractForm drizzle={drizzle} contract="Hello" method="set" />
+//         // </div>
+//       )
+//     }}
+//   </DrizzleContext.Consumer>
+// }
 
+// export default App;
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default () => (
   <DrizzleContext.Consumer>
     {drizzleContext => {
@@ -17,7 +41,7 @@ export default () => (
         //console.log(drizzleState)
         <div>
         <AccountData drizzle={drizzle} drizzleState={drizzleState} accountIndex={0} units="ether" precision={3}/>
-        {/* <ContractData drizzle={drizzle} drizzleState={drizzleState} contract="Hello" method="get"/> */}
+        <ContractData drizzle={drizzle} drizzleState={drizzleState} contract="Hello" method="get"/>
         <ContractForm drizzle={drizzle} contract="Hello" method="set" />
         </div>
       )

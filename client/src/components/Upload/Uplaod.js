c39b0acbe5376dde61f08517e37e9e6342ce3463
Abhinav Nakarmi <nakarmi.mail@gmail.com>
2022-04-22 00:49:42 +0530

    form

--- client/src/components/Upload/Uplaod.js
+++ client/src/components/Upload/Uplaod.js
@@ -1,19 +1,15 @@
 import React from 'react'
 import './Upload.scss'
-import {images} from "../../constants" 
+import { images } from "../../constants"
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Input from '@mui/material/Input';
-// const Input = styled('input')({
-//   display: 'none',
-// });
-
 
 function Login() {
   return (
-<div className="app__header app__flex">
-  <div className="app__header-badge">
+    <div className="app__header app__flex">
+      <div className="app__header-badge">
         <div className="badge-cmp app__flex">
           <div style={{ marginLeft: 20 }}>
             <p className="p-text">Save you credentials forever.</p>
@@ -21,42 +17,36 @@ function Login() {
           </div>
         </div>
 
-      <div className="tag-cmp">
-        <Box
-        component="form"
-        sx={{
-          '& .MuiTextField-root': { m: 1, width: '25ch' },
-        }}
-        >
+        <div className="tag-cmp">
+          <form>
+            <TextField
+              id="outlined-basic"
+              label="Name"
+              type="text"
+            />
 
-        <TextField
-          id="outlined-basic"
-          label="Name"
-          type="text"
-        />
-    
-        <TextField
-          id="outlined-basic"
-          label="Batch"
-          type="text"
-        />
-        <br></br>
-        <br></br>
-        <label> <h3>Choose the File to upload: </h3></label>
-        <Input type="file" id="myFile" />
-        <br></br>
-        <br></br>
-        <Button type="submit" variant="contained">Submit</Button>
-        </Box>
-        
+            <TextField
+              id="outlined-basic"
+              label="Batch"
+              type="text"
+            />Í
+            <br></br>
+            <br></br>
+            <label> <h3>Choose the File to upload: </h3></label>
+            <Input type="file" id="myFile" />
+            <br></br>
+            <br></br>
+            <Button type="submit" variant="contained">Submit</Button>
+          </form>
+
+        </div>
       </div>
+
+
+      <img src={images.node} alt="profile_bg" />
+
+
     </div>
-   
-
-    <img src={images.node} alt="profile_bg" />
-
-        
-  </div>
   )
 }
 

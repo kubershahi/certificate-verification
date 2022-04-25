25626d6e1f7794a8116b67c4832858029d05e496
Kuber Shahi <shahikuber97@gmail.com>
2022-04-26 01:23:08 +0530

    Create Certificate.sol

---
+++ contracts/Certificate.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.13;
+
+contract certificate {
+    address private owner;
+    mapping(string => string) private merkleRoot;
+
+    event OwnerSet(address indexed oldOwner, address indexed newOwner);
+
+    modifier isOwner() {
+        require(msg.sender == owner, "Caller is not owner");
+        _;
+    }
+
+    constructor() {
+        // set the contract deployer as the owner
+        owner = msg.sender;
+        emit OwnerSet(address(0), owner); // log the change in the onership
+    }
+
+    function changeOwner(address newOwner) public isOwner {
+        emit OwnerSet(owner, newOwner);
+        owner = newOwner;
+    }
+
+    function getOwner() external view returns (address) {
+        return owner;
+    }
+
+    function addRoot(string memory batch, string memory root) public isOwner {
+        merkleRoot[batch] = root;
+    }
+
+    function verifyRoot(string memory batch, string memory root)
+        public
+        view
+        returns (bool)
+    {
+        bool veriState;
+        if (keccak256(bytes(merkleRoot[batch])) == keccak256(bytes(root))) {
+            veriState = true;
+        } else {
+            veriState = false;
+        }
+        return veriState;
+    }
+}

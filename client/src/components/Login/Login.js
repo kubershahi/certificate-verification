af6288f70d762acb9decbed59e221f2b2f86cfd5
Kuber Shahi <shahikuber97@gmail.com>
2022-04-27 19:38:12 +0530

    Update Login.js

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,27 +1,46 @@
 import React from 'react'
 import './Login.scss'
-import {images} from "../../constants" 
+import { images } from "../../constants"
+
+import { DrizzleContext } from "@drizzle/react-plugin";
+import { newContextComponents } from "@drizzle/react-components";
+const { AccountData, ContractData, ContractForm } = newContextComponents;
+
+
 function Login() {
   return (
-<div className="app__header app__flex">
-      <div className="app__header-badge">
-        <div className="badge-cmp app__flex">
-          <div style={{ marginLeft: 20 }}>
-            <p className="p-text">Login</p>
-            <h1 className="head-texter">We make it possible!</h1>
-          </div>
+    // <div className="app__header app__flex">
+    //   <div className="app__header-badge">
+    //     <div className="badge-cmp app__flex">
+    //       <div style={{ marginLeft: 20 }}>
+    //         <p className="p-text">Login</p>
+    //         <h1 className="head-texter">We make it possible!</h1>
+    //       </div>
+    //     </div>
+
+    //     <div className="tag-cmp app__flex">
+    //       <p className="p-text">See below</p>
+    //     </div>
+    //   </div>
+  
+    // <img src={images.git} alt="profile_bg" /> 
+    // </div>
+
+    <DrizzleContext.Consumer>
+    {drizzleContext => {
+      const { drizzle, drizzleState, initialized } = drizzleContext;      
+      if (!initialized) {
+        return "Loading...";
+      }
+      return (
+        <div>
+        <AccountData drizzle={drizzle} drizzleState={drizzleState} accountIndex={0} units="ether" precision={3}/>
+        <ContractData drizzle={drizzle} drizzleState={drizzleState} contract="Certificate" method="test"/>
+        {/* <ContractForm drizzle={drizzle} contract="Hello" method="set" /> */}
         </div>
-
-        <div className="tag-cmp app__flex">
-          <p className="p-text">See below</p>
-        </div>
-      </div>
-   
-
-      {/* <img src={images.git} alt="profile_bg" /> */}
-
-
-  </div>
+      )
+    }}
+    </DrizzleContext.Consumer>
   )
 }
 

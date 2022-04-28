98407c488af638d5d047a0a8420cb81dc65670fa
Kuber Shahi <shahikuber97@gmail.com>
2022-04-28 06:49:23 +0530

    Update Verify.js

--- client/src/components/Verify/Verify.js
+++ client/src/components/Verify/Verify.js
@@ -33,56 +33,6 @@ function Verify(props) {
       var leaf = sha3.getHash("HEX")
       setUser({ ...user, fileHash: leaf });
     };
-  };
-
-  function timeout(delay) {
-    return new Promise( res => setTimeout(res, delay) );
-  }
-
-
-  const getRoot = (batch) => {
-
-    console.log(batch)
-    let root;
-
-    if (!props.drizzleState) return "Waiting for State Initialization";
-    const { drizzle, drizzleState } = props
-    console.log(drizzle)
-    console.log(drizzleState.drizzleStatus.initialized)
-    if (drizzleState.drizzleStatus.initialized) {
-
-          const contract = drizzle.contracts.Certificate;
-      
-          const dataKey = contract.methods.getRoot.cacheCall(batch)
-          console.log(dataKey)
-
-
-          var check = function(){
-            if(drizzleState.contracts.Certificate.getRoot[dataKey]){
-              root = drizzleState.contracts.Certificate.getRoot[dataKey].value
-            }
-            else {
-              setTimeout(check, 1000); // check again in a second
-            }
-        }
-
-          // var status = await drizzleState.contracts.Certificate.getRoot[dataKey] 
-
-          // console.log(drizzleState.contracts.Certificate.getRoot[dataKey]) 
-          // if(drizzleState.contracts.Certificate.getRoot[dataKey]){
-          //   console.log(drizzleState.contracts.Certificate.getRoot[dataKey].value)
-          //   return drizzleState.contracts.Certificate.getRoot[dataKey].value
-          // } 
-    }
-
-  }
-
-  const PostData = async (e) => {
-    e.preventDefault();
-
-    // console.log(user.name)
-    // console.log(user.batch)
-    // console.log(user.fileHash)
 
     axios.get("http://localhost:4000/getCertificate/", { params: { "name": user.name, "batch": user.batch } })
       .then((response) => {
@@ -102,15 +52,79 @@ function Verify(props) {
           }
 
           var root = getRoot(user.batch)
-          console.log(root)
+          // console.log(root)
 
           const verified = verifyProof(user.fileHash, root, proof)
-          console.log(verified)
+          // console.log(verified)
         }
       })
       .catch((error) => {
         console.log(error);
       });
+
+  };
+
+  const getRoot = (batch) => {
+
+    // console.log(batch)
+    let root;
+
+    if (!props.drizzleState) return "Waiting for State Initialization";
+    const { drizzle, drizzleState } = props
+    // console.log(drizzle)
+    // console.log(drizzleState.drizzleStatus.initialized)
+    if (drizzleState.drizzleStatus.initialized) {
+
+      const contract = drizzle.contracts.Certificate;
+
+      const dataKey = contract.methods.getRoot.cacheCall(batch)
+
+      // var status = drizzleState.contracts.Certificate.getRoot[dataKey]
+      // console.log(status)
+      if(drizzleState.contracts.Certificate.getRoot[dataKey]){
+        root = drizzleState.contracts.Certificate.getRoot[dataKey].value
+      }
+    }
+    return root
+
+  }
+
+  const [success,setSuccess] = useState("");
+  const PostData = async (e) => {
+    e.preventDefault();
+
+    axios.get("http://localhost:4000/getCertificate/", { params: { "name": user.name, "batch": user.batch } })
+      .then((response) => {
+        if (response.data.length === 0) {
+          console.log("Can't Find the certificate.")
+        } else {
+          let userData = response.data[0]
+          let certificates = userData["certificate"]
+          // let root = userData["merkleRoot"]
+          let cert_length = Object.keys(certificates).length
+
+          for (i = 0; i < cert_length; i++) {
+            if (user.fileHash === certificates[i]["hash"]) {
+              // Leaf of the pdf file
+              var proof = certificates[i]["proof"]
+            }
+          }
+
+          var root = getRoot(user.batch)
+
+          const verified = verifyProof(user.fileHash, root, proof)
+          if(verified){
+            console.log("fileHash: ", user.fileHash)
+            console.log("merkleProof: ", proof)
+            console.log("merkleRoot: ", root)
+          }
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+      setSuccess("Verified");
+
   }
 
   const areAllFieldsFilled = ((user.fileHash !== "") && (user.name !== "") && (user.batch !== ""));
@@ -162,6 +176,7 @@ function Verify(props) {
               Submit
             </Button>
           </form>
+          {success}
         </div>
       </div>
 

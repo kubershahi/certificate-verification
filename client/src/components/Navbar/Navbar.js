af890d1569ae7764c67996cd232790535030fd6a
Kuber Shahi <shahikuber97@gmail.com>
2022-04-28 06:57:02 +0530

    Update Navbar.js

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -10,7 +10,7 @@ function Navbar() {
         <p>aCERT</p>
       </div>
       <ul className="app__navbar-links">
-        {['home', 'upload','verify','share','login'].map((item) => (
+        {['home', 'upload','verify'].map((item) => (
           <li className="app__flex p-text" key={`link-${item}`}>
             <div />
             <a href={`/${item}`}>{item}</a>
